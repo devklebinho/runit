74430a713ebcf0533d71e0bc33428b95749a7b55
devklebinho <contato@klebersilva.dev.br>
2024-05-13 17:57:23 -0300

    runit new env

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Biblioteca\Desktop\KLEBER\runit\venv
+set VIRTUAL_ENV=C:\Users\Biblioteca\Desktop\KLEBER\Python\runit\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
